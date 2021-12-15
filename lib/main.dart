8738009fb05535676a6e2c52490711d5947a99df
MDAcst <mara.acst@hotmail.com>
2021-12-15 00:20:18 -0300

    datos API Ok

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,21 @@
+// ignore_for_file: unused_import, prefer_typing_uninitialized_variables, avoid_print, missing_return, unused_local_variable
+
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart'; as http;
+import 'package:http/http.dart' as http;
+import 'package:tareaclase14_datos_api/models/reqres_model.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
@@ -20,24 +25,56 @@ class MyApp extends StatelessWidget {
   }
 }
 
-Future getUsuarios(){
-final resp = await http.get('https://reqres.in/api/users'); 
+Future<ReqResRespuesta> getUsuarios() async {
+  final resp = await http.get(Uri.parse('https://reqres.in/api/users'));
+  return reqResRespuestaFromJson(resp.body);
 }
 
 class MiPagina extends StatelessWidget {
-  const MiPagina({Key? key}) : super(key: key);
+  const MiPagina({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.orange,
         title: const Text('Datos API'),
+        centerTitle: true,
       ),
-      body: Center(
-        child: Container(
-          child: Text('Datos API'),
-        ),
-      ),
+      body: FutureBuilder(
+          future: getUsuarios(),
+          builder:
+              (BuildContext context, AsyncSnapshot<ReqResRespuesta> snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(child: CircularProgressIndicator());
+            } else {
+              return _ListaUsuarios(snapshot.data.data);
+            }
+          }),
     );
+  }
+}
+
+class _ListaUsuarios extends StatelessWidget {
+  final List<Usuario> usuarios;
+
+  const _ListaUsuarios(this.usuarios);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        itemCount: usuarios.length,
+        itemBuilder: (BuildContext context, int i) {
+          final usuario = usuarios[i];
+
+          return FadeIn(
+            delay: Duration(milliseconds: 100 * i),
+            child: ListTile(
+              title: Text('${usuario.firstName} ${usuario.lastName}'),
+              subtitle: Text(usuario.email),
+              trailing: Image.network(usuario.avatar),
+            ),
+          );
+        });
   }
 }
